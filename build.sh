24d1b5463b957d592294fadc83c92b5fcd04897d
Ed Maste <emaste@freebsd.org>
2019-02-11 17:03:23 -0500

    Save objdump output for each object file

--- build.sh
+++ build.sh
@@ -4,5 +4,9 @@ set -e
 for CC in clang gcc; do
 	$CC -Wall -fPIC -c -o lib_pic_$CC.o lib.c
 	$CC -Wall -fPIE -c -o lib_pie_$CC.o lib.c
+
+	objdump -r -S lib_pic_$CC.o > lib_pic_$CC.txt
+	objdump -r -S lib_pie_$CC.o > lib_pie_$CC.txt
+
 	PATH=/usr/local/bin:$PATH diffoscope --html $CC.html lib_pic_$CC.o lib_pie_$CC.o || true
 done
