123d243e3b40ef42f09848e2fecd6a590a8c4545
Ed Maste <emaste@freebsd.org>
2019-02-12 12:05:51 -0500

    Also add a linked version to test linker pie behaviour

--- build.sh
+++ build.sh
@@ -5,8 +5,11 @@ for CC in clang gcc; do
 	for ptype in pie pic; do
 		PTYPE=$(echo $ptype | tr a-z A-Z)
 		$CC -Wall -f$PTYPE -c -o lib_${ptype}_$CC.o lib.c
+		$CC -Wall -f$PTYPE -o ${ptype}_$CC lib_${ptype}_$CC.o main.c
 		objdump -r -S lib_${ptype}_$CC.o > lib_${ptype}_$CC.txt
+		objdump -r -S ${ptype}_$CC > ${ptype}_$CC.txt
 	done
 
 	PATH=/usr/local/bin:$PATH diffoscope --html $CC.html lib_pic_$CC.o lib_pie_$CC.o || true
+	PATH=/usr/local/bin:$PATH diffoscope --html linked_$CC.html pic_$CC pie_$CC || true
 done
---
+++ main.c
@@ -0,0 +1,7 @@
+extern int lib_function2(int);
+
+int
+main(int argc, char *argv[])
+{
+	return (lib_function2(argc));
+}
